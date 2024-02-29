f4fe28d7477774cc826dd35838dcf3f75338a90c
Prajwal G Majjigi <prajwal1majjigi@gmail.com>
2024-02-29 12:29:18 +0530

    Authentication for the app

--- src/index.js
+++ src/index.js
@@ -1,27 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ProductProvider } from './context/ProductContext';
-import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-import Modal from 'react-modal'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ContextProvider } from "./Context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-Modal.setAppElement("#root");
- 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <ProductProvider>
-    <ToastContainer />
-     <App />
-  </ProductProvider>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
