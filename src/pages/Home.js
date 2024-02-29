2587005f2846ab6bafe7abe9a0f966799e84c1a6
Prajwal G Majjigi <prajwal1majjigi@gmail.com>
2024-02-29 12:56:10 +0530

    commited all changes making the cart user specific

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,12 @@
-import React,{useState,useEffect} from 'react'
-import Navbar from '../components/Navbar/Navbar'
-import ProductList from '../components/Product/ProductList'
-import SongList from '../components/Product/SongList'
-import AddProductForm from '../components/AddProductForm'
+import React from "react";
+import HomeLayout from "../Components/Home/HomeLayout";
 
-
-const Home = () => {
-
-    const [movies,setMovies] = useState([])
-    const [isLoading,setisLoading] = useState(true)
-
-    const fetchData = async () => {
-        try {
-          const response = await fetch('https://swapi.dve/api/films');
-          const data = await response.json();
-          setMovies(data);
-          setisLoading(false)
-        } catch (error) {
-          console.error('Error fetching movies:', error.message);
-        }
-      };
-      
-    useEffect(() => {
-        fetchData();
-
-    },[])
-       
+function Home() {
   return (
-    <div className="w-full min-h-screen">
-         <div className="flex ">
-             <AddProductForm />
-         </div>
-          <button className="bg-black text-white font-serif p-3 m-2 text-xl ml-10 items-center h-fit  border-2 rounded-lg hover:bg-white hover:text-black hover:border-black" onClick={()=>{fetchData()}}>Fetch Movies</button>
-    </div>
-  )
+    <>
+      <HomeLayout />
+    </>
+  );
 }
 
-export default Home
+export default Home;
